c3ec8f763606fdc631df2f708b742348681c7621
Honey Narendran <honey.narendran3@gmail.com>
2017-06-23 21:06:06 +0530

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
@@ -27,26 +29,14 @@ io.on('connection', (socket) => {
         createdAt: new Date().getTime()
     });
 
-    socket.broadcast.emit("newMessage", {
-        from: "admin",
-        text: "New user joined!",
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit("newMessage", generateMessage("Admin", "New user joined!"));
 
     socket.on('createMessage', (message) => {
         console.log('New message', message);
 
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage( message.from, message.text));
 
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createAt: new Date().getTime()
-        // });
+        // socket.broadcast.emit('newMessage', generateMessage( message.from, message.text));
     });  
 });
 
