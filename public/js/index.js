db10d16e41082fa0cc159aa8fad259621621f200
Honey Narendran <honey.narendran3@gmail.com>
2017-06-30 14:14:44 +0530

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('New message', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    var template = jQuery("#message-template").html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    jQuery('#messages').append(li);
+    jQuery("#messages").append(html);
 });
 
 socket.on('newLocationMessage', function (location) {
     var formattedTime = moment(location.createdAt).format('h: mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${location.from} ${formattedTime}: `);
-    a.attr('href', location.url)
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template = jQuery("#locationMessage-template").html();
+    var html = Mustache.render(template, {
+        from: location.from,
+        createdAt: formattedTime,
+        link: location.url
+    });
+
+    jQuery("#messages").append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
