e76e4d92232026c518b2ee71c7ae815c83ade565
Honey Narendran <honey.narendran3@gmail.com>
2017-06-30 12:25:56 +0530

    Add timestamps using moment

---
+++ playground/time.js
@@ -0,0 +1,14 @@
+// // Jan 1st 1970 00:00:00 am
+
+ var moment = require('moment');
+
+// var date = moment();
+// //date.add(1, 'year');
+
+// console.log(date.format('MMM Do, YYYY '));
+
+// // 10.35 am
+
+var date = moment();
+
+console.log(date.format('h:m a'));
